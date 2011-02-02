afff8d4be6e6e2c68b3f4df7fab0132c260a049e
Patrick Ruhkopf <patman@patman.local>
2011-02-01 20:41:31 -0500

    fixed build props

--- project/build/invsys.scala
+++ project/build/invsys.scala
@@ -2,12 +2,20 @@ import sbt._
 
 class InvSysProject(info: ProjectInfo) extends DefaultWebProject(info)
 {
-  val snapshots = ScalaToolsSnapshots
-  val lift = "net.liftweb" %% "lift-mapper" % "2.2-SNAPSHOT" % "compile"
-  val jetty6 = "org.mortbay.jetty" % "jetty" % "6.1.25" % "test"
-  // val h2 = "com.h2database" % "h2" % "1.2.121" % "runtime"
-  // alternately use derby
-  // val derby = "org.apache.derby" % "derby" % "10.2.2.0" % "runtime"
-  val servlet = "javax.servlet" % "servlet-api" % "2.5" % "provided"
-  val junit = "junit" % "junit" % "3.8.1" % "test"    
+  val liftVersion = "2.2"
+
+  // uncomment the following if you want to use the snapshot repo
+  // val scalatoolsSnapshot = ScalaToolsSnapshots
+
+  // If you're using JRebel for Lift development, uncomment
+  // this line
+  // override def scanDirectories = Nil
+
+  override def libraryDependencies = Set(
+    "net.liftweb" %% "lift-webkit" % liftVersion % "compile->default",
+    "org.mortbay.jetty" % "jetty" % "6.1.22" % "test->default",
+    "junit" % "junit" % "4.5" % "test->default",
+    "ch.qos.logback" % "logback-classic" % "0.9.26",
+    "org.scala-tools.testing" %% "specs" % "1.6.6" % "test->default"
+  ) ++ super.libraryDependencies
 }
