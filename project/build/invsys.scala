9038c2d56fb53827d93a5e8b809de759b83dab52
patman <patman@10.0.1.38>
2011-02-01 21:36:25 -0500

    fixed eclipse build issue

--- project/build/invsys.scala
+++ project/build/invsys.scala
@@ -1,8 +1,7 @@
 import sbt._
 import de.element34.sbteclipsify._
 
-class InvSysProject(info: ProjectInfo) extends DefaultWebProject(info) with Eclipsify
-{
+class InvSysProject(info: ProjectInfo) extends DefaultWebProject(info) with Eclipsify {
   val liftVersion = "2.2"
 
   // uncomment the following if you want to use the snapshot repo
@@ -12,11 +11,16 @@ class InvSysProject(info: ProjectInfo) extends DefaultWebProject(info) with Ecli
   // this line
   // override def scanDirectories = Nil
 
+  override def unmanagedClasspath =
+    super.unmanagedClasspath +++ buildCompilerJar
+
+  override def filterScalaJars = false
+
   override def libraryDependencies = Set(
     "net.liftweb" %% "lift-webkit" % liftVersion % "compile->default",
     "org.mortbay.jetty" % "jetty" % "6.1.22" % "test->default",
     "junit" % "junit" % "4.5" % "test->default",
     "ch.qos.logback" % "logback-classic" % "0.9.26",
-    "org.scala-tools.testing" %% "specs" % "1.6.6" % "test->default"
-  ) ++ super.libraryDependencies
+    "org.scala-lang" % "scala-compiler" % "2.8.1" % "compile->default",
+    "org.scala-tools.testing" %% "specs" % "1.6.6" % "test->default") ++ super.libraryDependencies
 }
