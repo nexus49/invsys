b61fc6f2adb376c6407d3f70747f4fdfa516c14e
patman <patman@10.0.1.38>
2011-02-01 20:56:44 -0500

    format

--- src/main/scala/pbc/snippet/HelloWorld.scala
+++ src/main/scala/pbc/snippet/HelloWorld.scala
@@ -1,14 +1,14 @@
 package pbc {
-package snippet {
+  package snippet {
 
-import _root_.scala.xml.NodeSeq
-import _root_.net.liftweb.util.Helpers
-import Helpers._
+    import _root_.scala.xml.NodeSeq
+    import _root_.net.liftweb.util.Helpers
+    import Helpers._
 
-class HelloWorld {
-  def howdy(in: NodeSeq): NodeSeq =
-    Helpers.bind("b", in, "time" -> (new _root_.java.util.Date).toString)
-}
+    class HelloWorld {
+      def howdy(in: NodeSeq): NodeSeq =
+        Helpers.bind("b", in, "time" -> (new _root_.java.util.Date).toString)
+    }
 
-}
+  }
 }
