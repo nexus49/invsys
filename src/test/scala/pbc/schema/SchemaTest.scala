023d6003186ed65c59cef69e7a4c0c6608784596
echt06 <bastian.echterhoelter@bertelsmann.de>
2011-02-08 09:11:46 -0500

    added second test

--- src/test/scala/pbc/schema/SchemaTest.scala
+++ src/test/scala/pbc/schema/SchemaTest.scala
@@ -1,6 +1,6 @@
 package pbc.schema
 
-import net.liftweb.common.{Loggable, Empty}
+import net.liftweb.common.{ Loggable, Empty }
 import scala.tools.nsc.util.trace
 import bootstrap.liftweb.Boot
 import net.liftweb.util.Props
@@ -9,7 +9,7 @@ import org.squeryl.adapters.H2Adapter
 import net.liftweb.squerylrecord.SquerylRecord
 import net.liftweb.mapper.{ StandardDBVendor, DefaultConnectionIdentifier, DB }
 import org.junit.Before
-import org.scalatest.junit.{AssertionsForJUnit, ShouldMatchersForJUnit}
+import org.scalatest.junit.{ AssertionsForJUnit, ShouldMatchersForJUnit }
 import org.junit.Test
 import org.squeryl.{ SessionFactory, Session }
 
@@ -31,17 +31,37 @@ class SchemaTest extends AssertionsForJUnit with ShouldMatchersForJUnit with Log
     }
   }
 
+  val testName: String = "Jim Carry"
   @Test
   def verifySelect() {
     DB.use(DefaultConnectionIdentifier) { conn =>
       {
         val author: Author = Author.createRecord
-        author.name.set("Jim Carry")
+        author.name.set(testName)
         Library.authors.insert(author)
 
         val newAuthor = from(Library.authors)(b => select(b)).single
         logger.info("Selected authors: %s".format(newAuthor.name.value))
-        newAuthor.name.value should be ("Jim Carry")
+        newAuthor.name.value should be(testName)
+      }
+    }
+  }
+
+  @Test
+  def verifyUpdate() {
+    import pbc.schema.Library._
+
+    verifySelect
+    DB.use(DefaultConnectionIdentifier) { conn =>
+      {
+    	val newName = "Hans Meier"
+        val author = from(Library.authors)(b => where(b.name.is === testName) select (b)).single
+        author.name.set(newName)
+        authors.update(author)
+        
+        val updatedAuthor = from(Library.authors)(b => select(b)).single
+        logger.info("Selected authors: %s".format(updatedAuthor.name.value))
+        updatedAuthor.name.value should be(newName)
       }
     }
   }
